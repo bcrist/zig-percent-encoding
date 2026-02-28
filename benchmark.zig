8f9923a6b3755362a0395daec9d57395e5c4774c
Ben Crist <github@magicmoremagic.com>
2026-02-28 14:59:07 -0600

    benchmark fixes

--- benchmark.zig
+++ benchmark.zig
@@ -1,15 +1,15 @@
 
-const min_content_length = 100 * 1024 * 1024;
-const total_iterations = 10;
-const warmup_iterations = 2;
+const min_content_length = 50 * 1024 * 1024;
+const total_iterations = 5;
+const warmup_iterations = 1;
 
 pub fn main() !void {
     defer std.debug.assert(.ok == gpa.deinit());
 
     var content = try std.ArrayList(u8).initCapacity(gpa.allocator(), min_content_length + 100);
-    defer content.deinit();
+    defer content.deinit(gpa.allocator());
     
-    var xoshiro: std.rand.Xoshiro256 = .{ .s = .{
+    var xoshiro: std.Random.Xoshiro256 = .{ .s = .{
         std.crypto.random.int(u64),
         std.crypto.random.int(u64),
         std.crypto.random.int(u64),
@@ -20,32 +20,34 @@ pub fn main() !void {
         const kind = rnd.float(f32);
         if (kind < 0.1) {
             // random byte
-            try content.append(rnd.int(u8));
+            try content.append(gpa.allocator(), rnd.int(u8));
         } else if (kind < 0.5) {
             // random reserved char
             const reserved = "!#$&\'()*+,/:;=?@[] ";
-            try content.append(reserved[rnd.intRangeLessThan(u8, 0, reserved.len)]);
+            try content.append(gpa.allocator(), reserved[rnd.intRangeLessThan(u8, 0, reserved.len)]);
         } else {
             // alphanumeric string
             const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
             for (0..100) |_| {
-                try content.append(alpha[rnd.intRangeLessThan(u8, 0, alpha.len)]);
+                try content.append(gpa.allocator(), alpha[rnd.intRangeLessThan(u8, 0, alpha.len)]);
                 if (rnd.float(f32) < 0.05) break;
             }
         }
     }
 
-    var temp = try std.ArrayList(u8).initCapacity(gpa.allocator(), min_content_length + 100);
+    var temp: std.io.Writer.Allocating = .init(gpa.allocator());
     defer temp.deinit();
 
-    var temp2 = try std.ArrayList(u8).initCapacity(gpa.allocator(), min_content_length + 100);
+    var temp2: std.io.Writer.Allocating = .init(gpa.allocator());
     defer temp2.deinit();
 
     for (0..total_iterations) |i| {
         temp.clearRetainingCapacity();
+        var list = temp.toArrayList();
+        defer temp = .fromArrayList(gpa.allocator(), &list);
 
         const begin = std.time.nanoTimestamp();
-        try percent_encoding.encode_append(&temp, content.items, .{ .spaces = .percent_encoded });
+        try percent_encoding.encode_append(gpa.allocator(), &list, content.items, .init(.default, .{ .spaces = .percent_encoded }));
         const end = std.time.nanoTimestamp();
 
         if (i >= warmup_iterations) {
@@ -58,26 +60,24 @@ pub fn main() !void {
 
     for (0..total_iterations) |i| {
         temp.clearRetainingCapacity();
-        const writer = temp.writer();
 
         const begin = std.time.nanoTimestamp();
-        try writer.print("{}", .{ percent_encoding.fmtEncoded(content.items) });
+        try temp.writer.print("{f}", .{ percent_encoding.fmt(content.items, .default) });
         const end = std.time.nanoTimestamp();
 
         if (i >= warmup_iterations) {
             const nanos: f64 = @floatFromInt(end - begin);
             const bytes: f64 = @floatFromInt(content.items.len);
             const nanos_per_byte = nanos / bytes;
-            std.debug.print("percent_encoding.fmtEncoded: {} ns/B\n", .{ nanos_per_byte });
+            std.debug.print("percent_encoding.fmt: {} ns/B\n", .{ nanos_per_byte });
         }
     }
 
     for (0..total_iterations) |i| {
         temp.clearRetainingCapacity();
-        const writer = temp.writer();
 
         const begin = std.time.nanoTimestamp();
-        try percent_encoding.encode_writer(writer, content.items, .{ .spaces = .percent_encoded });
+        try percent_encoding.encode_writer(&temp.writer, content.items, .init(.default, .{ .spaces = .percent_encoded }));
         const end = std.time.nanoTimestamp();
 
         if (i >= warmup_iterations) {
@@ -90,10 +90,9 @@ pub fn main() !void {
 
     for (0..total_iterations) |i| {
         temp.clearRetainingCapacity();
-        const writer = temp.writer();
 
         const begin = std.time.nanoTimestamp();
-        try std.Uri.Component.percentEncode(writer, content.items, is_valid);
+        try std.Uri.Component.percentEncode(&temp.writer, content.items, is_valid);
         const end = std.time.nanoTimestamp();
 
         if (i >= warmup_iterations) {
@@ -106,10 +105,10 @@ pub fn main() !void {
 
     for (0..total_iterations) |i| {
         temp2.clearRetainingCapacity();
-        try temp2.appendSlice(temp.items);
+        try temp2.writer.writeAll(temp.written());
 
         const begin = std.time.nanoTimestamp();
-        const result = percent_encoding.decode_in_place(temp2.items, .{ .decode_plus_as_space = false });
+        const result = percent_encoding.decode_in_place(temp2.written(), .{ .decode_plus_as_space = false });
         const end = std.time.nanoTimestamp();
 
         var checksum: usize = 0;
@@ -127,10 +126,10 @@ pub fn main() !void {
 
     for (0..total_iterations) |i| {
         temp2.clearRetainingCapacity();
-        try temp2.appendSlice(temp.items);
+        try temp2.writer.writeAll(temp.written());
 
         const begin = std.time.nanoTimestamp();
-        const result = std.Uri.percentDecodeInPlace(temp2.items);
+        const result = std.Uri.percentDecodeInPlace(temp2.written());
         const end = std.time.nanoTimestamp();
 
         var checksum: usize = 0;
@@ -148,7 +147,7 @@ pub fn main() !void {
 }
 
 fn is_valid(c: u8) bool {
-    return !percent_encoding.Encode_Options.should_encode(.{}, c);
+    return !percent_encoding.Encode_Options.should_encode(.default, c);
 }
 
 var gpa: std.heap.GeneralPurposeAllocator(.{}) = .{};
